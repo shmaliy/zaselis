c28de72acc5c29fa3aaae5e1e6795699396bfc71
maxim <shmaliy.maxim@gmail.com>
2013-06-20 22:20:05 +0300

    321

--- public/cms/db.php
+++ public/cms/db.php
@@ -7,7 +7,7 @@ switch ($_SERVER['HTTP_HOST']) {
         $cms_config_password = '';
         $cms_config_db = 'zaselis';
         break;
-    case 'dev.zaselis.com':
+    case 'new.zaselis.com':
         $cms_config_host = 'localhost';
         $cms_config_user = 'zaselisc_dev';
         $cms_config_password = '1q2w3e4r5t';

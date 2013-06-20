819987f5e1d10807847bdbe773df1f0386a58946
maxim <shmaliy.maxim@gmail.com>
2013-06-20 21:44:30 +0300

    sdf

--- public/cms/db.php
+++ public/cms/db.php
@@ -1,14 +1,19 @@
 <?php 
 
-$cms_config_host = 'localhost';
-$cms_config_user = 'u2714_wt';
-$cms_config_password = '1q2w3e4r5t';
-$cms_config_db = 'u2714_wt';
-
-$cms_config_host = 'localhost';
-$cms_config_user = 'root';
-$cms_config_password = '';
-$cms_config_db = 'zaselis';
+switch ($_SERVER['HTTP_HOST']) {
+    case 'public.zaselis_new':
+        $cms_config_host = 'localhost';
+        $cms_config_user = 'root';
+        $cms_config_password = '';
+        $cms_config_db = 'zaselis';
+        break;
+    case 'dev.zaselis.com':
+        $cms_config_host = 'localhost';
+        $cms_config_user = 'zaselisc_dev';
+        $cms_config_password = '1q2w3e4r5t';
+        $cms_config_db = 'zaselisc_dev';
+        break;
+}
 
 $cms_config_dbprefix = 'cms';
 ?>
